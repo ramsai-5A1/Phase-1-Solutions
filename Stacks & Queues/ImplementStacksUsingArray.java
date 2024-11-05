ec51abdaee81b90908661a97d521f089199fd0e1
Ramsai <ramsaicrony@gmail.com>
2024-11-05 15:21:02 +0530

    Added Stacks & Queues

---
+++ Stacks & Queues/ImplementStacksUsingArray.java
@@ -0,0 +1,26 @@
+
+
+class MyStack {
+    private int[] arr;
+    private int top;
+
+    public MyStack() {
+        arr = new int[1000];
+        top = -1;
+    }
+
+    public void push(int x) {
+        // Your Code
+        
+        top = top+1;
+        arr[top]=x;
+    }
+
+    public int pop() {
+        // Your Code
+        if(top==-1) return -1;
+        int val = arr[top];
+        top=top-1;
+        return val;
+    }
+}
