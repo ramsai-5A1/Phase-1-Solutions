4ae6847f1197194388f268faf021b56ec3e4d97d
Ramsai <ramsaicrony@gmail.com>
2024-11-05 15:19:49 +0530

    Linked List

---
+++ LinkedList/ConstructLinkedList.java
@@ -0,0 +1,26 @@
+
+
+// User function Template for Java
+
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node() { data = 0; }
+    Node(int d) { data = d; }  //constructor to create a new node
+}
+*/
+class Solution {
+    static Node constructLL(int arr[]) {
+        Node head = new Node(arr[0]);
+        Node mover = head;
+        for(int i=1;i<arr.length;i++){
+            Node temp = new Node(arr[i]);
+            mover.next=temp;
+            mover = temp;
+        }
+        return head;
+        
+    }
+}
